223c6a29a7c693616b65570e7710fba1f42989b5
Aleksei Sazonov <s.aleksey@russianit.ru>
2016-10-18 18:12:14 +0400

    tmp

--- src/main/groovy/ct0/DefaultProperty.groovy
+++ src/main/groovy/ct0/DefaultProperty.groovy
@@ -1,7 +1,5 @@
 package ct0
 
-import acmi.l2.clientmod.l2resources.Sysstr
-import acmi.l2.clientmod.util.IOUtil
 import acmi.l2.clientmod.util.UIEntity
 import groovy.beans.Bindable
 import groovy.transform.CompileStatic
@@ -11,66 +9,64 @@ import groovy.transform.CompileStatic
 abstract class DefaultProperty implements UIEntity {
     String name
     String superName = 'undefined'
-    Boolean alwaysOnTop
-    Boolean alwaysOnBack
-    String parentName
-    String preOrder = 'undefined'
-    String postOrder = 'undefined'
-    Boolean anchored
-    Boolean size = false
-    Boolean size_absolute_values = true
-    String size_percent_window
+    int unk2 = -1
+    int unk3 = -1
+    String unk4
+    String unk5 = 'undefined'
+    String unk6 = 'undefined'
+    int unk7 = -1
+    boolean size = false
+    boolean size_absolute_values = true
     float size_percent_width
     float size_percent_height
     int size_absolute_width
     int size_absolute_height
-    Boolean usePosition = false
-    Alignment relativePoint = Alignment.NONE
-    Alignment anchorPoint = Alignment.NONE
-    String relativeTo
+    boolean anchor = false
+    Alignment anchor_parent = Alignment.NONE
+    Alignment anchor_this = Alignment.NONE
+    String anchor_ctrl
     int anchor_x
     int anchor_y
     int unk22
-    int virtual
+    int unk23
     int unk24
-    String tooltipType = 'undefined'
-    @Sysstr
-    int tooltipText = -9999
+    String popupType = 'undefined'
+    int popupValue = -9999
 
     @Override
     DefaultProperty read(InputStream input) {
         name = input.readString()
         superName = input.readString()
-        alwaysOnTop = input.readInt()
-        alwaysOnBack = input.readInt()
-        parentName = input.readString()
-        preOrder = input.readString()
-        postOrder = input.readString()
-        anchored = input.readInt()
+        unk2 = input.readInt()
+        unk3 = input.readInt()
+        unk4 = input.readString()
+        unk5 = input.readString()
+        unk6 = input.readString()
+        unk7 = input.readInt()
         size = input.readBoolean()
         if (size) {
             size_absolute_values = input.readBoolean()
             if (!size_absolute_values) {
-                size_percent_window = input.readString()
+                input.readCompactInt()                 //TODO unk empty array
                 size_percent_width = input.readFloat()
                 size_percent_height = input.readFloat()
             }
             size_absolute_width = input.readInt()
             size_absolute_height = input.readInt()
         }
-        usePosition = input.readBoolean()
-        if (usePosition) {
-            relativePoint = Alignment.values()[input.readInt()]
-            anchorPoint = Alignment.values()[input.readInt()]
-            relativeTo = input.readString()
+        anchor = input.readBoolean()
+        if (anchor) {
+            anchor_parent = Alignment.values()[input.readInt()]
+            anchor_this = Alignment.values()[input.readInt()]
+            anchor_ctrl = input.readString()
             anchor_x = input.readInt()
             anchor_y = input.readInt()
         }
         unk22 = input.readInt()
-        virtual = input.readInt()
+        unk23 = input.readInt()
         unk24 = input.readInt()
-        tooltipType = input.readString()
-        tooltipText = input.readInt()
+        popupType = input.readString()
+        popupValue = input.readInt()
 
         this
     }
@@ -79,36 +75,36 @@ abstract class DefaultProperty implements UIEntity {
     DefaultProperty write(OutputStream output) {
         output.writeString(name)
         output.writeString(superName)
-        output.writeBoolean(alwaysOnTop)
-        output.writeBoolean(alwaysOnBack)
-        output.writeString(parentName)
-        output.writeString(preOrder)
-        output.writeString(postOrder)
-        output.writeBoolean(anchored)
+        output.writeInt(unk2)
+        output.writeInt(unk3)
+        output.writeString(unk4)
+        output.writeString(unk5)
+        output.writeString(unk6)
+        output.writeInt(unk7)
         output.writeBoolean(size)
         if (size) {
             output.writeBoolean(size_absolute_values)
             if (!size_absolute_values) {
-                output.writeString(size_percent_window)
+                output.writeCompactInt(0)                    //TODO unk empty array
                 output.writeFloat(size_percent_width)
                 output.writeFloat(size_percent_height)
             }
             output.writeInt(size_absolute_width)
             output.writeInt(size_absolute_height)
         }
-        output.writeBoolean(usePosition)
-        if (usePosition) {
-            output.writeInt(relativePoint.ordinal())
-            output.writeInt(anchorPoint.ordinal())
-            output.writeString(relativeTo)
+        output.writeBoolean(anchor)
+        if (anchor) {
+            output.writeInt(anchor_parent.ordinal())
+            output.writeInt(anchor_this.ordinal())
+            output.writeString(anchor_ctrl)
             output.writeInt(anchor_x)
             output.writeInt(anchor_y)
         }
         output.writeInt(unk22)
-        output.writeInt(virtual)
+        output.writeInt(unk23)
         output.writeInt(unk24)
-        output.writeString(tooltipType)
-        output.writeInt(tooltipText)
+        output.writeString(popupType)
+        output.writeInt(popupValue)
 
         this
     }
@@ -117,72 +113,4 @@ abstract class DefaultProperty implements UIEntity {
     String toString() {
         "$name[${getClass().simpleName}]"
     }
-
-    //@formatter:off
-    @Deprecated String getSuperName() { superName }
-    @Deprecated void setSuperName(String superName) { this.superName = superName }
-
-    @Deprecated int getUnk2() { IOUtil.boolToInt(alwaysOnTop) }
-    @Deprecated void setUnk2(int unk2) { this.alwaysOnTop = IOUtil.intToBool(unk2) }
-
-    @Deprecated int getUnk3() { IOUtil.boolToInt(alwaysOnBack) }
-    @Deprecated void setUnk3(int unk3) { this.alwaysOnBack = IOUtil.intToBool(unk3) }
-
-    @Deprecated String getUnk4() { parentName }
-    @Deprecated void setUnk4(String unk4) { this.parentName = unk4 }
-
-    @Deprecated String getUnk5() { preOrder }
-    @Deprecated void setUnk5(String unk5) { this.preOrder = unk5 }
-
-    @Deprecated String getUnk6() { postOrder }
-    @Deprecated void setUnk6(String unk6) { this.postOrder = unk6 }
-
-    @Deprecated int getUnk7() { IOUtil.boolToInt(anchored) }
-    @Deprecated void setUnk7(int unk7) { this.anchored = IOUtil.intToBool(unk7) }
-
-    @Deprecated boolean getSize() { size }
-    @Deprecated void setSize(boolean size) { this.size = size }
-
-    @Deprecated boolean getSize_absolute_values() { size_absolute_values }
-    @Deprecated void setSize_absolute_values(boolean size_absolute_values) { this.size_absolute_values = size_absolute_values }
-
-    @Deprecated float getSize_percent_width() { size_percent_width }
-    @Deprecated void setSize_percent_width(float size_percent_width) { this.size_percent_width = size_percent_width }
-
-    @Deprecated float getSize_percent_height() { size_percent_height }
-    @Deprecated void setSize_percent_height(float size_percent_height) { this.size_percent_height = size_percent_height }
-
-    @Deprecated int getSize_absolute_width() { size_absolute_width }
-    @Deprecated void setSize_absolute_width(int size_absolute_width) { this.size_absolute_width = size_absolute_width }
-
-    @Deprecated int getSize_absolute_height() { size_absolute_height }
-    @Deprecated void setSize_absolute_height(int size_absolute_height) { this.size_absolute_height = size_absolute_height }
-
-    @Deprecated boolean getAnchor() { usePosition }
-    @Deprecated void setAnchor(boolean anchor) { this.usePosition = anchor }
-
-    @Deprecated Alignment getAnchor_parent() { relativePoint }
-    @Deprecated void setAnchor_parent(Alignment anchor_parent) { this.relativePoint = anchor_parent }
-
-    @Deprecated Alignment getAnchor_this() { anchorPoint }
-    @Deprecated void setAnchor_this(Alignment anchor_this) { this.anchorPoint = anchor_this }
-
-    @Deprecated String getAnchor_ctrl() { relativeTo }
-    @Deprecated void setAnchor_ctrl(String anchor_ctrl) { this.relativeTo = anchor_ctrl }
-
-    @Deprecated int getAnchor_x() { anchor_x }
-    @Deprecated void setAnchor_x(int anchor_x) { this.anchor_x = anchor_x }
-
-    @Deprecated int getAnchor_y() { anchor_y }
-    @Deprecated void setAnchor_y(int anchor_y) { this.anchor_y = anchor_y }
-
-    @Deprecated int getUnk23() { virtual }
-    @Deprecated void setUnk23(int unk23) { this.virtual = unk23 }
-
-    @Deprecated String getPopupType() { tooltipType }
-    @Deprecated void setPopupType(String popupType) { this.tooltipType = popupType }
-
-    @Deprecated int getPopupValue() { tooltipText }
-    @Deprecated void setPopupValue(int popupValue) { this.tooltipText = popupValue }
-    //@formatter:on
 }
--- src/main/groovy/ct0/Window.groovy
+++ src/main/groovy/ct0/Window.groovy
@@ -2,6 +2,8 @@ package ct0
 
 import acmi.l2.clientmod.l2resources.Sysstr
 import acmi.l2.clientmod.l2resources.Tex
+import acmi.l2.clientmod.util.ArrayLength
+import acmi.l2.clientmod.util.Hide
 import acmi.l2.clientmod.util.IOUtil
 import acmi.l2.clientmod.util.IntValue
 import acmi.l2.clientmod.util.Type
@@ -28,28 +30,34 @@ class Window extends DefaultProperty implements Iterable<DefaultProperty> {
     @Sysstr
     int title
     Boolean resizeFrame
-    FrameSizeType frameSize = FrameSizeType.None
-    DirectionType frameDirection = DirectionType.None
+    int frameSize// = FrameSizeType.None
+    int frameDirection// = DirectionType.None
     Boolean exitbutton
     Boolean draggable
-    DirectionType resizeFrameDirection = DirectionType.None
+    int resizeFrameDirection// = DirectionType.None
     int resizeFrameX
     int resizeFrameY
     float resizeFrameWidth
     float resizeFrameHeight
     int resizeMaxIncrease
-    DirectionType drawerDirection = DirectionType.None
+    int drawerDirection
     int offsetX
     int offsetY
-    Boolean directionFixed
+    int directionFixed
+//    @Hide
+//    int notUsed01 // offsetX
+//    @Hide
+//    int notUsed02 // offsetY
+//    @Hide
+//    int notUsed03 // directionFixed
     int unk127
     int unk128
     String ownerWindow
-    AnimType showAnimType = AnimType.None
-    AnimType hideAnimType = AnimType.None
-    DirectionType showAnimDirection = DirectionType.None
+    int showAnimType// = AnimType.None
+    int hideAnimType// = AnimType.None
+    int showAnimDirection// = DirectionType.None
     int showAnimSeconds
-    DirectionType hideAnimDirection = DirectionType.None
+    int hideAnimDirection// = DirectionType.None
     int hideAnimSeconds
     @Tex
     String iconName
@@ -120,119 +128,216 @@ class Window extends DefaultProperty implements Iterable<DefaultProperty> {
         }
     }
 
+    @Override
+    Window read(InputStream input) throws IOException {
+        super.read(input)
+        
+        parent = input.readString()
+        backTex = input.readString()
+        script = input.readString()
+        state = input.readString()
+        frame = input.readBoolean()
+        iconable = input.readBoolean()
+        stuckable = input.readBoolean()
+        hidden = input.readBoolean()
+        alwaysFullAlpha = input.readBoolean()
+        savePosition = input.readBoolean()
+        saveSize = input.readBoolean()
+        title = input.readInt()
+        resizeFrame = input.readBoolean()
+        frameSize = input.readInt()
+        frameDirection = input.readInt()
+        exitbutton = input.readBoolean()
+        draggable = input.readBoolean()
+        resizeFrameDirection = input.readInt()
+        resizeFrameX = input.readInt()
+        resizeFrameY = input.readInt()
+        resizeFrameWidth = input.readFloat()
+        resizeFrameHeight = input.readFloat()
+        resizeMaxIncrease = input.readInt()
+        drawerDirection = input.readInt()
+        offsetX = input.readInt()
+        offsetY =input.readInt()
+        directionFixed = input.readInt()
+        unk127 = input.readInt()
+        unk128 = input.readInt()
+        ownerWindow = input.readString()
+        showAnimType = input.readInt()
+        hideAnimType = input.readInt()
+        showAnimDirection = input.readInt()
+        showAnimSeconds = input.readInt()
+        hideAnimDirection = input.readInt()
+        hideAnimSeconds =  input.readInt()
+        iconName = input.readString()
+        tooltipIdx = input.readInt()
+        hookKeyInput = input.readBoolean()
+        workingConfiguration = input.readString()
+        
+        this
+    }
+
+    @Override
+    Window write(OutputStream output) throws IOException {
+        super.write(output)
+        
+        output.writeString(parent)
+        output.writeString(backTex)        
+        output.writeString(script)        
+        output.writeString(state)        
+        output.writeBoolean(frame)        
+        output.writeBoolean(iconable)        
+        output.writeBoolean(stuckable)        
+        output.writeBoolean(hidden)        
+        output.writeBoolean(alwaysFullAlpha)        
+        output.writeBoolean(savePosition)        
+        output.writeBoolean(saveSize)        
+        output.writeInt(title)        
+        output.writeBoolean(resizeFrame)
+        output.writeInt(frameSize)
+        output.writeInt(frameDirection)
+        output.writeBoolean(exitbutton)        
+        output.writeBoolean(draggable)
+        output.writeInt(resizeFrameDirection)
+        output.writeInt(resizeFrameX)        
+        output.writeInt(resizeFrameY)        
+        output.writeFloat(resizeFrameWidth)        
+        output.writeFloat(resizeFrameHeight)        
+        output.writeInt(resizeMaxIncrease)        
+        output.writeInt(drawerDirection)        
+        output.writeInt(offsetX)        
+        output.writeInt(offsetY)        
+        output.writeInt(directionFixed)        
+        output.writeInt(unk127)        
+        output.writeInt(unk128)        
+        output.writeString(ownerWindow)        
+        output.writeInt(showAnimType)        
+        output.writeInt(hideAnimType)        
+        output.writeInt(showAnimDirection)        
+        output.writeInt(showAnimSeconds)        
+        output.writeInt(hideAnimDirection)        
+        output.writeInt(hideAnimSeconds)        
+        output.writeString(iconName)        
+        output.writeInt(tooltipIdx)        
+        output.writeBoolean(hookKeyInput)        
+        output.writeString(workingConfiguration)        
+        output.writeList(children)  
+        
+        this
+    }
+
     // @formatter:off
-    @Deprecated String getUnk100() { parent }
-    @Deprecated void setUnk100(String unk100) { this.parent = unk100 }
+//    @Deprecated String getUnk100() { parent }
+//    @Deprecated void setUnk100(String unk100) { this.parent = unk100 }
+//
+//    @Deprecated String getUnk101() { backTex }
+//    @Deprecated void setUnk101(String unk101) { this.backTex = unk101 }
+//
+//    @Deprecated String getUnk102() { script }
+//    @Deprecated void setUnk102(String unk102) { this.script = unk102 }
+//
+//    @Deprecated String getUnk103() { state }
+//    @Deprecated void setUnk103(String unk103) { this.state = unk103 }
+//
+//    @Deprecated int getUnk104() { IOUtil.boolToInt(frame) }
+//    @Deprecated void setUnk104(int unk104) { this.frame = IOUtil.intToBool(unk104) }
+//
+//    @Deprecated int getUnk105() { IOUtil.boolToInt(iconable) }
+//    @Deprecated void setUnk105(int unk105) { this.iconable = IOUtil.intToBool(unk105) }
+//
+//    @Deprecated int getUnk106() { IOUtil.boolToInt(stuckable) }
+//    @Deprecated void setUnk106(int unk106) { this.stuckable = IOUtil.intToBool(unk106) }
+//
+//    @Deprecated int getUnk107() { IOUtil.boolToInt(hidden) }
+//    @Deprecated void setUnk107(int unk107) { this.hidden = IOUtil.intToBool(unk107) }
+//
+//    @Deprecated int getUnk108() { IOUtil.boolToInt(alwaysFullAlpha) }
+//    @Deprecated void setUnk108(int unk108) { this.alwaysFullAlpha = IOUtil.intToBool(unk108) }
+//
+//    @Deprecated int getUnk109() { IOUtil.boolToInt(savePosition) }
+//    @Deprecated void setUnk109(int unk109) { this.savePosition = IOUtil.intToBool(unk109) }
+//
+//    @Deprecated int getUnk110() { IOUtil.boolToInt(saveSize) }
+//    @Deprecated void setUnk110(int unk110) { this.saveSize = IOUtil.intToBool(unk110) }
+//
+//    @Deprecated int getUnk111() { title }
+//    @Deprecated void setUnk111(int unk111) { this.title = unk111 }
+//
+//    @Deprecated int getUnk112() { IOUtil.boolToInt(resizeFrame) }
+//    @Deprecated void setUnk112(int unk112) { this.resizeFrame = IOUtil.intToBool(unk112) }
 
-    @Deprecated String getUnk101() { backTex }
-    @Deprecated void setUnk101(String unk101) { this.backTex = unk101 }
-
-    @Deprecated String getUnk102() { script }
-    @Deprecated void setUnk102(String unk102) { this.script = unk102 }
-
-    @Deprecated String getUnk103() { state }
-    @Deprecated void setUnk103(String unk103) { this.state = unk103 }
-
-    @Deprecated int getUnk104() { IOUtil.boolToInt(frame) }
-    @Deprecated void setUnk104(int unk104) { this.frame = IOUtil.intToBool(unk104) }
-
-    @Deprecated int getUnk105() { IOUtil.boolToInt(iconable) }
-    @Deprecated void setUnk105(int unk105) { this.iconable = IOUtil.intToBool(unk105) }
-
-    @Deprecated int getUnk106() { IOUtil.boolToInt(stuckable) }
-    @Deprecated void setUnk106(int unk106) { this.stuckable = IOUtil.intToBool(unk106) }
-
-    @Deprecated int getUnk107() { IOUtil.boolToInt(hidden) }
-    @Deprecated void setUnk107(int unk107) { this.hidden = IOUtil.intToBool(unk107) }
-
-    @Deprecated int getUnk108() { IOUtil.boolToInt(alwaysFullAlpha) }
-    @Deprecated void setUnk108(int unk108) { this.alwaysFullAlpha = IOUtil.intToBool(unk108) }
-
-    @Deprecated int getUnk109() { IOUtil.boolToInt(savePosition) }
-    @Deprecated void setUnk109(int unk109) { this.savePosition = IOUtil.intToBool(unk109) }
-
-    @Deprecated int getUnk110() { IOUtil.boolToInt(saveSize) }
-    @Deprecated void setUnk110(int unk110) { this.saveSize = IOUtil.intToBool(unk110) }
-
-    @Deprecated int getUnk111() { title }
-    @Deprecated void setUnk111(int unk111) { this.title = unk111 }
-
-    @Deprecated int getUnk112() { IOUtil.boolToInt(resizeFrame) }
-    @Deprecated void setUnk112(int unk112) { this.resizeFrame = IOUtil.intToBool(unk112) }
-
-    @Deprecated int getUnk113() { frameSize.ordinal() }
-    @Deprecated void setUnk113(int unk113) { this.frameSize = FrameSizeType.valueOf(unk113) }
-
-    @Deprecated int getUnk114() { frameDirection.ordinal() }
-    @Deprecated void setUnk114(int unk114) { this.frameDirection = DirectionType.valueOf(unk114) }
-
-    @Deprecated int getUnk115() { IOUtil.boolToInt(exitbutton) }
-    @Deprecated void setUnk115(int unk115) { this.exitbutton = IOUtil.intToBool(unk115) }
-
-    @Deprecated int getUnk116() { IOUtil.boolToInt(draggable) }
-    @Deprecated void setUnk116(int unk116) { this.draggable = IOUtil.intToBool(unk116) }
-
-    @Deprecated int getUnk117() { resizeFrameDirection.ordinal() }
-    @Deprecated void setUnk117(int unk117) { this.resizeFrameDirection = DirectionType.valueOf(unk117) }
-
-    @Deprecated int getUnk118() { resizeFrameX }
-    @Deprecated void setUnk118(int unk118) { this.resizeFrameX = unk118 }
-
-    @Deprecated int getUnk119() { resizeFrameY }
-    @Deprecated void setUnk119(int unk119) { this.resizeFrameY = unk119 }
-
-    @Deprecated float getUnk120() { resizeFrameWidth }
-    @Deprecated void setUnk120(float unk120) { this.resizeFrameWidth = unk120 }
-
-    @Deprecated float getUnk121() { resizeFrameHeight }
-    @Deprecated void setUnk121(float unk121) { this.resizeFrameHeight = unk121 }
-
-    @Deprecated int getUnk122() { resizeMaxIncrease }
-    @Deprecated void setUnk122(int unk122) { this.resizeMaxIncrease = unk122 }
-
-    @Deprecated int getUnk123() { drawerDirection.ordinal() }
-    @Deprecated void setUnk123(int unk123) { this.drawerDirection = DirectionType.valueOf(unk123) }
-
-    @Deprecated int getUnk124() { offsetX }
-    @Deprecated void setUnk124(int unk124) { this.offsetX = unk124 }
-
-    @Deprecated int getUnk125() { offsetY }
-    @Deprecated void setUnk125(int unk125) { this.offsetY = unk125 }
-
-    @Deprecated int getUnk126() { IOUtil.boolToInt(directionFixed) }
-    @Deprecated void setUnk126(int unk126) { this.directionFixed = IOUtil.intToBool(unk126) }
-
-    @Deprecated String getUnk129() { ownerWindow }
-    @Deprecated void setUnk129(String unk129) { this.ownerWindow = unk129 }
-
-    @Deprecated int getUnk130() { showAnimType.ordinal() }
-    @Deprecated void setUnk130(int unk130) { this.showAnimType = AnimType.valueOf(unk130) }
-
-    @Deprecated int getUnk131() { hideAnimType.ordinal() }
-    @Deprecated void setUnk131(int unk131) { this.hideAnimType = AnimType.valueOf(unk131) }
-
-    @Deprecated int getUnk132() { showAnimDirection.ordinal() }
-    @Deprecated void setUnk132(int unk132) { this.showAnimDirection = DirectionType.valueOf(unk132) }
-
-    @Deprecated int getUnk133() { showAnimSeconds }
-    @Deprecated void setUnk133(int unk133) { this.showAnimSeconds = unk133 }
-
-    @Deprecated int getUnk134() { hideAnimDirection.ordinal() }
-    @Deprecated void setUnk134(int unk134) { this.hideAnimDirection = DirectionType.valueOf(unk134) }
-
-    @Deprecated int getUnk135() { hideAnimSeconds }
-    @Deprecated void setUnk135(int unk135) { this.hideAnimSeconds = unk135 }
-
-    @Deprecated String getUnk136() { iconName }
-    @Deprecated void setUnk136(String unk136) { this.iconName = unk136 }
-
-    @Deprecated int getUnk137() { tooltipIdx }
-    @Deprecated void setUnk137(int unk137) { this.tooltipIdx = unk137 }
-
-    @Deprecated int getUnk138() { IOUtil.boolToInt(hookKeyInput) }
-    @Deprecated void setUnk138(int unk138) { this.hookKeyInput = IOUtil.intToBool(unk138) }
-
-    @Deprecated String getUnk139() { workingConfiguration }
-    @Deprecated void setUnk139(String unk139) { this.workingConfiguration = unk139 }
+//    @Deprecated int getUnk113() { frameSize.ordinal() }
+//    @Deprecated void setUnk113(int unk113) { this.frameSize = FrameSizeType.valueOf(unk113) }
+//
+//    @Deprecated int getUnk114() { frameDirection.ordinal() }
+//    @Deprecated void setUnk114(int unk114) { this.frameDirection = DirectionType.valueOf(unk114) }
+//
+//    @Deprecated int getUnk115() { IOUtil.boolToInt(exitbutton) }
+//    @Deprecated void setUnk115(int unk115) { this.exitbutton = IOUtil.intToBool(unk115) }
+//
+//    @Deprecated int getUnk116() { IOUtil.boolToInt(draggable) }
+//    @Deprecated void setUnk116(int unk116) { this.draggable = IOUtil.intToBool(unk116) }
+//
+//    @Deprecated int getUnk117() { resizeFrameDirection.ordinal() }
+//    @Deprecated void setUnk117(int unk117) { this.resizeFrameDirection = DirectionType.valueOf(unk117) }
+//
+//    @Deprecated int getUnk118() { resizeFrameX }
+//    @Deprecated void setUnk118(int unk118) { this.resizeFrameX = unk118 }
+//
+//    @Deprecated int getUnk119() { resizeFrameY }
+//    @Deprecated void setUnk119(int unk119) { this.resizeFrameY = unk119 }
+//
+//    @Deprecated float getUnk120() { resizeFrameWidth }
+//    @Deprecated void setUnk120(float unk120) { this.resizeFrameWidth = unk120 }
+//
+//    @Deprecated float getUnk121() { resizeFrameHeight }
+//    @Deprecated void setUnk121(float unk121) { this.resizeFrameHeight = unk121 }
+//
+//    @Deprecated int getUnk122() { resizeMaxIncrease }
+//    @Deprecated void setUnk122(int unk122) { this.resizeMaxIncrease = unk122 }
+//
+//    @Deprecated int getUnk123() { drawerDirection.ordinal() }
+//    @Deprecated void setUnk123(int unk123) { this.drawerDirection = DirectionType.valueOf(unk123) }
+//
+//    @Deprecated int getUnk124() { offsetX }
+//    @Deprecated void setUnk124(int unk124) { this.offsetX = unk124 }
+//
+//    @Deprecated int getUnk125() { offsetY }
+//    @Deprecated void setUnk125(int unk125) { this.offsetY = unk125 }
+//
+//    @Deprecated int getUnk126() { IOUtil.boolToInt(directionFixed) }
+//    @Deprecated void setUnk126(int unk126) { this.directionFixed = IOUtil.intToBool(unk126) }
+//
+//    @Deprecated String getUnk129() { ownerWindow }
+//    @Deprecated void setUnk129(String unk129) { this.ownerWindow = unk129 }
+//
+//    @Deprecated int getUnk130() { showAnimType.ordinal() }
+//    @Deprecated void setUnk130(int unk130) { this.showAnimType = AnimType.valueOf(unk130) }
+//
+//    @Deprecated int getUnk131() { hideAnimType.ordinal() }
+//    @Deprecated void setUnk131(int unk131) { this.hideAnimType = AnimType.valueOf(unk131) }
+//
+//    @Deprecated int getUnk132() { showAnimDirection.ordinal() }
+//    @Deprecated void setUnk132(int unk132) { this.showAnimDirection = DirectionType.valueOf(unk132) }
+//
+//    @Deprecated int getUnk133() { showAnimSeconds }
+//    @Deprecated void setUnk133(int unk133) { this.showAnimSeconds = unk133 }
+//
+//    @Deprecated int getUnk134() { hideAnimDirection.ordinal() }
+//    @Deprecated void setUnk134(int unk134) { this.hideAnimDirection = DirectionType.valueOf(unk134) }
+//
+//    @Deprecated int getUnk135() { hideAnimSeconds }
+//    @Deprecated void setUnk135(int unk135) { this.hideAnimSeconds = unk135 }
+//
+//    @Deprecated String getUnk136() { iconName }
+//    @Deprecated void setUnk136(String unk136) { this.iconName = unk136 }
+//
+//    @Deprecated int getUnk137() { tooltipIdx }
+//    @Deprecated void setUnk137(int unk137) { this.tooltipIdx = unk137 }
+//
+//    @Deprecated int getUnk138() { IOUtil.boolToInt(hookKeyInput) }
+//    @Deprecated void setUnk138(int unk138) { this.hookKeyInput = IOUtil.intToBool(unk138) }
+//
+//    @Deprecated String getUnk139() { workingConfiguration }
+//    @Deprecated void setUnk139(String unk139) { this.workingConfiguration = unk139 }
     // @formatter:on
 }
